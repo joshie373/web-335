154a940094ef7bac037b072432bb6e569684660b
Joshua Hughes <joshie373@gmail.com>
2019-12-03 17:48:52 -0700

    adding 8.2

---
+++ week-8/hughes_hello_world.py
@@ -0,0 +1,8 @@
+# ============================================
+# ; Title: Assignment 8.2
+# ; Author: Joshua Hughes
+# ; Date: 3 December 2019
+# ; Modified By: Joshua Hughes
+# ; Description: Introduction to Anaconda
+# ;===========================================
+print('Hello World')
