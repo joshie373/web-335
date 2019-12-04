11c2f5378adee75b09cfe0ead29f9539dc3308f3
Joshua Hughes <joshie373@gmail.com>
2019-12-03 18:05:21 -0700

    adding 8.2

---
+++ week-8/hughes_calculator.py
@@ -0,0 +1,27 @@
+# ============================================
+# ; Title: Assignment 8.3
+# ; Author: Joshua Hughes
+# ; Date: 3 December 2019
+# ; Modified By: Joshua Hughes
+# ; Description: Demonstrates basic
+# ; python operation
+# ;===========================================
+
+# function that takes two parameters(numbers) and adds them together
+def add(num1,num2):
+  sum = num1 + num2
+  return sum
+
+# function that takes two parameters(numbers) and finds the difference
+def subtract(num1,num2):
+  diff = num1 - num2
+  return diff
+
+# function that takes two parameters(numbers) and finds the quotient
+def divide(num1,num2):
+  quot = num1/num2
+  return quot
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
